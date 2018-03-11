c068caac7c2a25a0a20a3ce22f32ee1f3ed9e362
PasterzM <mateusz.ppasternak@gmail.com>
2018-03-11 20:39:37 +0100

    random
    Generowanie randomowo budynek, jeden jest obiektem przez ktory nie mozesz przechodzic a drugi niestety jest tylko tworzony

---
+++ Assets/ChooseBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseBuilding : MonoBehaviour {
+
+    public Vector3 offset;
+    public GameObject[] prefabs;
+    Transform objPos;
+
+    // Use this for initialization
+    void Start() {
+        int len = prefabs.Length;
+        GameObject obj = Instantiate(prefabs[Random.Range(0, len)]);
+        objPos = obj.GetComponent<Transform>();
+        objPos.position = transform.position + offset;
+    }
+
+    private void Update() {
+        objPos.position = transform.position + offset;
+    }
+}
